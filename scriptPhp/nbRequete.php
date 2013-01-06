bf71265742b4afe790e48d5852c4b4d6289941b5
Jonathan play <Jonathan.play@hotmail.fr>
2013-01-06 19:34:06 +0000

    Adaptation du code au nombre de requ?te

---
+++ scriptPhp/nbRequete.php
@@ -0,0 +1,19 @@
+<?php
+	$date1 = $_GET['date1'];
+	$date2 = $_GET['date2'];
+	echo '[';
+	mysql_connect("localhost", "root", "root") or die('Erreur de connexion avec la base de données');
+	mysql_select_db("serverLogs") or die('Base de données inexistante');
+	
+	$query = "select count(*) 'reqget' from apache_access_log where request_type = 'get' and ltime >='$date1' and ltime <='$date2'";
+	$r = mysql_query($query);
+	$a = mysql_fetch_object($r);
+	$res = $a->reqget;
+	echo "{\"title\":\"get\",\"value\":$res},";
+	
+	$query = "select count(*) 'reqoptions' from apache_access_log where request_type = 'options' and ltime >='$date1' and ltime <='$date2'";
+	$r = mysql_query($query);
+	$a = mysql_fetch_object($r);
+	$res = $a->reqoptions;
+	echo "{\"title\":\"options\",\"value\":$res},";
+?>
