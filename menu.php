c5c7230db1bb7e14243dd3ea82743c9b288d4d7d
baptiste-boulay <baptiste-boulay@hotmail.fr>
2013-01-26 16:27:10 +0000

    liens

--- menu.php
+++ menu.php
@@ -9,35 +9,35 @@
 			<li class="menu1">
 				<a class="menu1" href="error.php">Erreurs</a>
 				<ul class="menuniveau2">
-					<li class="menuniveau2"><a href="error.php">Nombre d'erreurs</a></li>
+					<li class="menuniveau2"><a class="menu11" href="error.php">Nombre d'erreurs</a></li>
 					<li class="menuniveau2"> type d'erreurs</li>
 				</ul>
 			</li>
 			<li class="menu2">
 				<a class="menu2" href="/Inscription.php">Accès</a>
 				<ul class="menuniveau2">
-					<li class="menuniveau2"> Periode d'accès</li>
-					<li class="menuniveau2"> Durée d'accès</li>
+              <li class="menuniveau2"> <a class="menu22" href="error.php">Periode d'accès</a></li>
+              <li class="menuniveau2"> <a class="menu22" href="error.php">Durée d'accès</a></li>
 				</ul>
 			</li>
 			<li class="menu3">
-				<a class="menu3" href="/Connexion.php">Requètes</a>	
+				<a class="menu3" href="requete.php">Requètes</a>	
 				<ul class="menuniveau2">
-					<li class="menuniveau2"> Type de requètes</li>
-					<li class="menuniveau2"> Nombre de requètes</li>
+					<li class="menuniveau2"><a class="menu33" href="requete.php"> Type de requètes</a></li>
+              <li class="menuniveau2"><a class="menu33" href="requete.php"> Nombre de requètes</a></li>
 				</ul>
 			</li>
 			<li class="menu4">
 				<a class="menu4" href="/Inscription.php">Agents</a>
 				<ul class="menuniveau2">
-					<li class="menuniveau2"> Nombre d'agents</li>
+              <li class="menuniveau2"><a class="menu44" href="requete.php"> Nombre d'agents</a></li>
 				</ul>
 			</li>
 			<li class="menu5">
 				<a class="menu5" href="/Inscription.php">Serveur</a>
 				<ul class="menuniveau2">
-					<li class="menuniveau2"> Statuts</li>
-					<li class="menuniveau2"> Taille des données</li>
+              <li class="menuniveau2"><a class="menu55" href="requete.php">Statuts</a></li>
+              <li class="menuniveau2"> <a class="menu55" href="requete.php">Taille des données</a></li>
 				</ul>
 			</li>
 		</ul>
