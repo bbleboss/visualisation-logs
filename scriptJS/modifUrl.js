7c98d777d461b2a409dc561cfd9cb26e599b897d
clement <clement.amblard@gmail.com>
2013-03-24 15:52:26 +0000

    modif date index

--- scriptJS/modifUrl.js
+++ scriptJS/modifUrl.js
@@ -1,12 +1,19 @@
 //-----------update des liens du menu pour transmettre les dates de page en page
 var date1 = document.getElementById('date1');
 var date2 = document.getElementById('date2');
+var url = document.location.href;
 
 
-date1.onchange = updateUrl;
+if(url.search('index.php'))
+{
+	transfertDateIndex();
+}
+else
+{
+	date1.onchange = updateUrl;
 			    
-date2.onchange = updateUrl;
-
+	date2.onchange = updateUrl;
+}
 function updateUrl(){
 				
 				var date1 = document.getElementById('date1');
@@ -35,8 +42,8 @@ function updateUrl(){
 					document.location.href = document.location.href+'#date1='+date1.value+'&date2='+date2.value;
 				}
 			    }
-//---------------Update des champs date1 et date2 + appel à update pour mettre à jour les liens au cas où l'utilisateur ne déclenche pas onchange pendant tout le temps où il est sur la page 
-var url = document.location.href;
+//---------------Update des champs date1 et date2 
+
 var pos = url.search('#');
 
 if(pos != -1)
@@ -58,4 +65,40 @@ function updateDate(date)
 	document.getElementById(date).value = date1;
 }
 
-updateUrl(date1, date2);
+
+
+function transfertDateIndex() { //fonction adapté pour la page d'index (qui n'a pas de formulaire de date) qui récupère les date dans l'url et modifie les liens
+	
+	pos = url.search('date1');
+	var date1 = '';
+	var i = pos+6;
+	while(url[i] != '&' && i < url.length)
+	{
+		date1 = date1 + url[i];
+		i++;
+	}
+	pos = url.search('date2');
+	var date2 = '';
+	var i = pos+6;
+	while(url[i] != '&' && i < url.length)
+	{
+		date2 = date2 + url[i];
+		i++;
+	}
+	liens = document.getElementById('menu').getElementsByTagName('a');//selection des liens
+	for(var i= 0; i < liens.length; i++)
+	{
+		var pos = liens[i].href.search('#');
+		if(pos != -1)
+		{
+			liens[i].href = liens[i].href.substring(0,pos);
+		}
+		liens[i].href = liens[i].href+'#date1='+date1+'&date2='+date2;//mise à jour du lien 
+					
+	}
+}
+//appel à update pour mettre à jour les liens au cas où l'utilisateur ne déclenche pas onchange pendant tout le temps où il est sur la page 
+if(!url.search('index.php'))
+{
+	updateUrl(date1, date2);
+}
