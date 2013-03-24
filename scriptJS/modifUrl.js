a665bf3b005c8afec4d7299b251f1435f859f12a
clement <clement.amblard@gmail.com>
2013-03-24 16:05:10 +0000

    modif date index

--- scriptJS/modifUrl.js
+++ scriptJS/modifUrl.js
@@ -3,8 +3,7 @@ var date1 = document.getElementById('date1');
 var date2 = document.getElementById('date2');
 var url = document.location.href;
 
-
-if(url.search('index.php'))
+if(url.search('index.php') != -1)
 {
 	transfertDateIndex();
 }
@@ -69,32 +68,35 @@ function updateDate(date)
 
 function transfertDateIndex() { //fonction adapté pour la page d'index (qui n'a pas de formulaire de date) qui récupère les date dans l'url et modifie les liens
 	
-	pos = url.search('date1');
-	var date1 = '';
-	var i = pos+6;
-	while(url[i] != '&' && i < url.length)
+	if(url.search('date1') != -1 && url.search('date2') != -1 )
 	{
-		date1 = date1 + url[i];
-		i++;
-	}
-	pos = url.search('date2');
-	var date2 = '';
-	var i = pos+6;
-	while(url[i] != '&' && i < url.length)
-	{
-		date2 = date2 + url[i];
-		i++;
-	}
-	liens = document.getElementById('menu').getElementsByTagName('a');//selection des liens
-	for(var i= 0; i < liens.length; i++)
-	{
-		var pos = liens[i].href.search('#');
-		if(pos != -1)
+		pos = url.search('date1');
+		var date1 = '';
+		var i = pos+6;
+		while(url[i] != '&' && i < url.length)
 		{
-			liens[i].href = liens[i].href.substring(0,pos);
+			date1 = date1 + url[i];
+			i++;
 		}
-		liens[i].href = liens[i].href+'#date1='+date1+'&date2='+date2;//mise à jour du lien 
+		pos = url.search('date2');
+		var date2 = '';
+		var i = pos+6;
+		while(url[i] != '&' && i < url.length)
+		{
+			date2 = date2 + url[i];
+			i++;
+		}
+		liens = document.getElementById('menu').getElementsByTagName('a');//selection des liens
+		for(var i= 0; i < liens.length; i++)
+		{
+			var pos = liens[i].href.search('#');
+			if(pos != -1)
+			{
+				liens[i].href = liens[i].href.substring(0,pos);
+			}
+			liens[i].href = liens[i].href+'#date1='+date1+'&date2='+date2;//mise à jour du lien 
 					
+		}
 	}
 }
 //appel à update pour mettre à jour les liens au cas où l'utilisateur ne déclenche pas onchange pendant tout le temps où il est sur la page 
